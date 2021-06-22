fd1289948dfb9a772ca29783b4150ab5c493aeeb
Patrick Tsang <scenage@gmail.com>
2021-06-22 12:41:58 +1000

    Removed all html pages and converted them to hbs pages.

--- src/app.js
+++ src/app.js
@@ -13,6 +13,20 @@ app.get('', (req,res) => {
     })
 })
 
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Patrick Tsang'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is a SOS'
+    })
+})
+
 app.get('/weather', (req,res) => {
     res.send({
         forecast: 'Clear',
