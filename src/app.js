2af9c3dd59c46f5d5d6d01b4dd8827593e62f9bc
Patrick Tsang <scenage@gmail.com>
2021-06-22 12:27:14 +1000

    Added handle bars.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../public')))
+app.set('view engine', 'hbs')
+
+app.get('', (req,res) => {
+    res.render('index')
+})
 
 app.get('/weather', (req,res) => {
     res.send({
